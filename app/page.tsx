1ca557cc61bc48bac3db70aa7e93ba597d4d1c41
Denim Basumatary <denimbasumatary@outlook.com>
2023-07-29 10:36:17 +0530

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,27 @@
 import Nav from "@/components/nav";
+import { LucideArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
 	return (
 		<div className="bg-black text-white min-h-screen flex flex-col">
 			<Nav />
-			<main className="flex-1 container mx-auto md:max-w-6xl px-4 grid place-content-center bg-hero bg-cover bg-left-top">
+			<main className="flex-1 container mx-auto md:max-w-6xl px-4 grid place-content-center bg-cover bg-left-top gap-8">
 				<div className="space-y-24">
-					<h2 className="font-bold text-8xl text-center leading-relaxed md:text-7xl">
+					<h2 className="font-bold text-8xl text-center leading-4 md:text-7xl">
 						Web3 Platform For Selling{" "}
 						<span className="text-accent">Digital Products</span>
 					</h2>
-					<div className="flex items-center justify-center gap-8">
-						<Link
-							href="https://fiqwy7k9btl.typeform.com/to/se6eiisR"
-							target="_blank"
-						>
-							<p className="text-lg hover:cursor-pointer hover:text-gray-400">
-								Early Access
-							</p>
-						</Link>
-						<Link href="https://discord.gg/DXn8rrnZaD" target="_blank">
-							<p className="text-lg hover:cursor-pointer hover:text-gray-400">
-								Join Discord
-							</p>
-						</Link>
-					</div>
+				</div>
+				<div className="place-self-center">
+					<Link href="/login">
+						<button className="bg-accent  text-black rounded-full px-5 py-3 text-sm font-medium">
+							<div className="flex items-center gap-2">
+								<p> start selling</p>
+								<LucideArrowRight width={16} height={16} />
+							</div>
+						</button>
+					</Link>
 				</div>
 			</main>
 		</div>
