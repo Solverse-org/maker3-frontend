218ff5398e0bd75f4fa340cfb9269954ba03dff0
Denim Basumatary <denimbasumatary@outlook.com>
2023-08-12 21:01:43 +0530

    custom input for select avatar

--- app/create-profile/page.tsx
+++ app/create-profile/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Logo from "@/components/Logo";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -9,29 +10,28 @@ import { useStorage } from "@thirdweb-dev/react";
 import Link from "next/link";
 import React, { FormEvent } from "react";
 
-interface Profile {
-	name: string;
-	email: string;
-	bio: string;
-}
-
 export default function CreateProfile() {
 	const [name, setName] = React.useState("");
 	const [email, setEmail] = React.useState("");
 	const [bio, setBio] = React.useState("");
+	const [avatarFile, setAvatarFile] = React.useState("");
+
+	const fileInputRef = React.useRef<HTMLInputElement>(null);
 
 	const storage = useStorage();
 
 	async function onSubmit(e: FormEvent) {
 		e.preventDefault();
-		console.log({ name, email, bio });
+		console.log({ name, email, bio, avatarFile });
 
 		// upload profile to uri
+		// TODO: call from server
 		if (!storage) return;
-		const profile: Profile = {
+		const profile = {
 			name,
 			email,
 			bio,
+			avatar: avatarFile,
 		};
 		const uri = await storage.upload(profile);
 		console.log({ uri });
@@ -53,7 +53,32 @@ export default function CreateProfile() {
 					<Card>
 						<CardHeader>Create profile</CardHeader>
 						<CardContent>
-							<form onSubmit={onSubmit} className="flex flex-col gap-2">
+							<form
+								onSubmit={onSubmit}
+								className="flex flex-col gap-2 w-[320px]"
+							>
+								<Avatar
+									className="place-self-center w-20 h-20 hover:cursor-pointer mb-3"
+									onClick={() => fileInputRef.current?.click()}
+								>
+									<AvatarImage src={avatarFile ?? ""} />
+									<AvatarFallback>
+										{name ? name.substring(0, 2).toUpperCase() : ""}
+									</AvatarFallback>
+								</Avatar>
+								<Input
+									ref={fileInputRef}
+									id="avatar"
+									type="file"
+									className="hidden"
+									onChange={(e) => {
+										const files = fileInputRef.current?.files;
+										if (!files) return;
+										const file = URL.createObjectURL(files[0]);
+										console.log({ file });
+										setAvatarFile(file);
+									}}
+								/>
 								<Input
 									type="text"
 									placeholder="Name"
@@ -71,7 +96,7 @@ export default function CreateProfile() {
 									value={bio}
 									onChange={(e) => setBio(e.target.value)}
 								/>
-								<Button>Create</Button>
+								<Button className="mt-2">Create</Button>
 							</form>
 						</CardContent>
 					</Card>
